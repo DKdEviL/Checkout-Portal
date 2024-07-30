2d192a6f0842995f22bef0773da3859649dc8db5
Deepak <deepaknardia98@gmail.com>
2024-07-30 01:09:38 +0100

    Boilerplate code

---
+++ src/main/java/com/example/checkoutportal/CheckoutPortalApplication.java
@@ -0,0 +1,13 @@
+package com.example.checkoutportal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CheckoutPortalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CheckoutPortalApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/checkoutportal/CheckoutPortalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.checkoutportal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CheckoutPortalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
