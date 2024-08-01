d41bdffa8e81acfad92885d7ad4c4fcafa7a7efa
Deepak <deepaknardia98@gmail.com>
2024-08-01 08:47:17 +0100

    Deleted Unnecessary files

--- src/main/java/com/example/checkoutportal/data/repository/DiscountRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.checkoutportal.data.repository;
-
-import com.example.checkoutportal.data.model.Discount;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface DiscountRepository extends JpaRepository<Discount, Long> {
-}
